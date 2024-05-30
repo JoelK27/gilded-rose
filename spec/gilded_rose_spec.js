30441fefea588ee32462b75623907af45ae8304b
Joel Kakkattu <if23b095@wien-technikum.at>
2024-05-30 17:59:52 +0200

    First working Unit test for legacy code

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,10 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should reduce quality and sell_in for normal items", function() {
+    items = [ new Item("Normal Item", 10, 20) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(19);
   });
 
 });
